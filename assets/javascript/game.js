dfe4fb372dc43064e778da7a2cf83ddd29c22c7b
Fred Nguyen <fred.h.nguyen@gmail.com>
2019-07-02 14:04:42 -0500

    pseudocoded the game

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+//computer chooses a random number between 19 and 120 to match
+
+//computer randomly generates a number between 1-12 for each gem
+
+//onclick add value of the gem to gemnum
+
+//if gem num > the computer number YOU LOSE
+
+//if gem num = the computer number YOU WIN
+
+//clear all variables and re generate randomnumbers.
+
